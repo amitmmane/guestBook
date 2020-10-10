db23fa08a99ee9bc1e64adc52a1bb66eff63e051
Amit Mane <amit.mane@bt.com>
2020-10-10 22:17:45 +0530

    Initial commit for project structure

---
+++ GuestBookDemo/src/main/java/com/example/guestbook/GuestBookDemoApplication.java
@@ -0,0 +1,13 @@
+package com.example.guestbook;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GuestBookDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GuestBookDemoApplication.class, args);
+	}
+
+}
